7482ef81b69e7ada421874c0296a51581f5534ab
neilsadev <neiloyhassan1@gmail.com>
2023-08-20 19:16:11 +0600

    additional formulas

--- lib/formula.dart
+++ lib/formula.dart
@@ -1,12 +1,40 @@
+import 'dart:math';
+
 class FormulaEvaluator {
   final Map<String, Function> _formulas = {
-    'SUM': (List<double> numbers) => numbers.reduce((a, b) => a + b),
-    'AVERAGE': (List<double> numbers) =>
+    'SUM': (List<num> numbers) => numbers.reduce((a, b) => a + b),
+    'AVERAGE': (List<num> numbers) =>
         numbers.reduce((a, b) => a + b) / numbers.length,
-    'COUNT': (List<double> numbers) => numbers.length.toDouble(),
-    'MAX': (List<double> numbers) => numbers.reduce((a, b) => a > b ? a : b),
-    'MIN': (List<double> numbers) => numbers.reduce((a, b) => a < b ? a : b),
-    // You might need to define more functions here
+    'COUNT': (List<num> numbers) => numbers.length,
+    'MAX': (List<num> numbers) => numbers.reduce((a, b) => a > b ? a : b),
+    'MIN': (List<num> numbers) => numbers.reduce((a, b) => a < b ? a : b),
+    'PRODUCT': (List<num> numbers) => numbers.reduce((a, b) => a * b),
+    'MEDIAN': (List<num> numbers) {
+      final sortedNumbers = [...numbers]..sort();
+      if (sortedNumbers.length % 2 == 1) {
+        return sortedNumbers[sortedNumbers.length ~/ 2];
+      } else {
+        final mid = sortedNumbers.length ~/ 2;
+        return (sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2;
+      }
+    },
+    'POWER': (List<num> numbers) =>
+        pow(numbers[0].toDouble(), numbers[1].toDouble()),
+    'SQRT': (List<num> numbers) => sqrt(numbers[0].toDouble()),
+    'LN': (List<num> numbers) => log(numbers[0].toDouble()),
+    'LOG': (List<num> numbers) => log(numbers[0].toDouble()) / ln10,
+    'EXP': (List<num> numbers) => exp(numbers[0].toDouble()),
+    'ABS': (List<num> numbers) => numbers[0].abs().toDouble(),
+    'ROUND': (List<num> numbers) => numbers[0].roundToDouble(),
+    'CEILING': (List<num> numbers) => numbers[0].ceilToDouble(),
+    'FLOOR': (List<num> numbers) => numbers[0].floorToDouble(),
+    'MOD': (List<num> numbers) => numbers[0] % numbers[1],
+    'SIN': (List<num> numbers) => sin(numbers[0].toDouble()),
+    'COS': (List<num> numbers) => cos(numbers[0].toDouble()),
+    'TAN': (List<num> numbers) => tan(numbers[0].toDouble()),
+    'ASIN': (List<num> numbers) => asin(numbers[0].toDouble()),
+    'ACOS': (List<num> numbers) => acos(numbers[0].toDouble()),
+    'ATAN': (List<num> numbers) => atan(numbers[0].toDouble()),
   };
 
   double evaluate(String formula) {
